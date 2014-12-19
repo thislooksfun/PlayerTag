935936bebea0d41416c9e0e19059dbc8b18dfa2f
thislooksfun <thislooksfun@repbot.org>
2014-12-18 20:42:30 -0600

    Updated version and finalized local repo

--- PlayerTag/src/com/tlf/playertag/util/Settings.java
+++ PlayerTag/src/com/tlf/playertag/util/Settings.java
@@ -7,7 +7,7 @@ public class Settings
 {
 	public static final String MODID = "playertag";
 	public static final String NAME = "Player Tag";
-	public static final String VERSION = "0.0.1 pre-alpha";
+	public static final String VERSION = "1.0.0";
 	
 	public static String ident()
 	{
